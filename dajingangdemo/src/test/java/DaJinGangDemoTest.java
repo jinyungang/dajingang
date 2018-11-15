2ea6a0b913df5f014a085b8a14633df6407437ad
jinYunGang <jinyungang@youyuwo.com>
2018-11-15 18:43:34 +0800

    update 大金刚

---
+++ dajingangdemo/src/test/java/DaJinGangDemoTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author JinYunGang
+ * @date 2018/11/15 18:41
+ */
+public class DaJinGangDemoTest {
+    public static void main(String[] args) {
+        System.out.println("大金刚");
+    }
+}
